abe543aab836358e331f5d1efa44105b6d61dfbb
Kazuya NUMATA <numata@gmail.com>
2013-03-15 15:31:10 +0900

    Implement the-building-blocks-of-lisp-syntax examples

---
+++ ch2/the-building-blocks-of-lisp-syntax.lisp
@@ -0,0 +1,25 @@
+(defun square (n)
+  (* n n))
+
+;; TEST
+(square 2)
+
+;; Symbols
+(eq 'fooo 'FoOo)
+
+;; Numbers
+(+ 1 1.0)
+
+(expt 53 53)
+
+(/ 4 6)
+
+(/ 4.0 6)
+
+;; String
+(princ "Tutti Frutti")
+
+(princ "He yelled \"Stop that thief!\" from the busy street.")
+
+
+

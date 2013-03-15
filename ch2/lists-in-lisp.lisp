87d8ddea4d380e99eeab0f50ff3645a9adc47a30
Kazuya NUMATA <numata@gmail.com>
2013-03-15 16:48:00 +0900

    Implement ch2/lists-in-lisp examples (in progress)

--- ch2/lists-in-lisp.lisp
+++ ch2/lists-in-lisp.lisp
@@ -31,4 +31,31 @@
 
 ;; the car and cdr functions
 
+(car '(pork beef chicken))
+
+(cdr '(pork beef chicken))
+
+(cdr '(pork beef chicken))
+(car '(beef chicken))
+(car (cdr '(pork beef chicken)))
+(cadr '(pork beef chicken))
+(caddr '(pork beef chicken))
+
+;; The list function
+
+(list 'pork 'beef 'chicken)
+
+;; Nested Lists
+'(cat (duck bat) ant)
+
+(car '((peas carrots tomatoes) (pork beef chicken)))
+(cdr '(peas carrots tomatoes))
+(cdr (car '((peas carrots tomatoes) (pork beef chicken))))
+(cdar'((peas carrots tomatoes) (pork beef chicken)))
+
+(cons (cons 'peas (cons 'carrots (cons 'tomatoes ())))
+      (cons (cons 'pork (cons 'beef (cons 'chicken ())))
+	    ()))
+
 ;; to be continued
+

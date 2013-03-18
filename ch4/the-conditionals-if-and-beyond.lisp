91d232d845f9bdfd9cfb71e80e8526a92bd6e1dc
Kazuya NUMATA <numata@gmail.com>
2013-03-18 10:58:07 +0900

    Implement  ch4/the-conditionals-if-and-beyond examples

--- ch4/the-conditionals-if-and-beyond.lisp
+++ ch4/the-conditionals-if-and-beyond.lisp
@@ -50,6 +50,30 @@
 
 ;; The Command That Does It All: cond
 
-;; to be continued
+(defvar *arch-enemy* nil)
+(defun pudding-eater (person)
+  (cond ((eq person 'henry) (setf *arch-enemy* 'stupid-lisp-alien)
+	 '(curse you lisp alien - you ate my pudding))
+	((eq person 'johnny) (setf *arch-enemy* 'useless-old-johnny)
+	 '(i hope you choked on my pudding johnny))
+	(t '(why you ate my pudding stranger ?))))
 
+;; TEST
+(pudding-eater 'johnny)
+*arch-enemy*
+(pudding-eater 'george-clooney)
+
+;; Branching with case
+
+(defun pudding-eater (person)
+  (case person
+    ((henry) (setf *arch-enemy* 'stupid-lisp-alien)
+     '(curse you lisp alien - you ate my pudding))
+    ((johnny) (setf *arch-enemy* 'useless-old-johnny)
+     '(i hope you choked on my pudding johnny))
+    (otherwise '(why you ate my pudding stranger ?))))
+;; TEST
+(pudding-eater 'johnny)
+*arch-enemy*
+(pudding-eater 'george-clooney)
 

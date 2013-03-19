845978095aa9d469ae1b4d90f13e34434bc81f82
Kazuya NUMATA <numata@gmail.com>
2013-03-19 12:51:09 +0900

    Implement ch4/cool-tricks-with-conditions examples

--- ch4/cool-tricks-with-conditions.lisp
+++ ch4/cool-tricks-with-conditions.lisp
@@ -25,5 +25,22 @@
 
 ;; Using Functions That Return More than Just the Truth
 
+(if (member 1 '(3 4 1 5))
+    'one-is-in-the-list
+    'one-is-not-int-the-list)
 
-;; to be continued
+(member 1 '(3 4 1 5))
+
+(if (member nil '(3 4 nil 5))
+    'nil-is-in-the-list
+    'nil-is-not-in-the-list)
+
+(find-if #'oddp '(2 4 5 6))
+
+(if (find-if #'oddp '(2 4 5 6))
+    'there-is-an-odd-number
+    'there-is-no-odd-number)
+
+(find-if #'null '(2 4 nil 6))
+;; NIL
+

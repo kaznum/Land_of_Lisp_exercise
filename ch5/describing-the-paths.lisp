9352429beb91e0f4ce49593199663033abccb25c
Kazuya NUMATA <numata@gmail.com>
2013-03-20 14:48:17 +0900

    Implement ch5/describing-the-paths examples (in progress)

--- ch5/describing-the-paths.lisp
+++ ch5/describing-the-paths.lisp
@@ -1,10 +1,26 @@
-(defparameter *edges '((living-room (garden west door)
-			(attic upstairs ladder))
-		       (garden (living-room east door))
-		       (attic (living-room downstairs ladder))))
+(defparameter *edges* '((living-room (garden west door)
+			 (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
 
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
 
 (describe-path '(garden west door))
+
+;; Describing Multiple Paths at Once
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(describe-paths 'living-room *edges*)
+
+(cdr (assoc 'living-room *edges*))
+
+(mapcar #'describe-path (cdr (assoc 'living-room *edges*)))
+
+(mapcar #'sqrt '(1 2 3 4 5))
+
+
+;; To be continued
+

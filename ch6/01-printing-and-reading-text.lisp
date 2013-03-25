5374ee7e0b756a7aca9cfcb5249926a3ffbe7684
Kazuya NUMATA <numata@gmail.com>
2013-03-25 18:19:42 +0900

    Implement ch6/01-printing-and-reading-text examples

--- ch6/01-printing-and-reading-text.lisp
+++ ch6/01-printing-and-reading-text.lisp
@@ -21,6 +21,49 @@
 (say-hello)
 
 ;; Starting with print and read
+(defun add-five ()
+  (print "please enter a number:")
+  (let ((num (read)))
+    (print "When I add five I get")
+    (print (+ num 5))))
 
-;; To be continued
+(add-five)
+
+(print '3)
+(print '3.4)
+(print 3.4)
+(print 'foo)
+(print "foo")
+(print '"foo")
+(print '#\a)
+
+(print '#\newline)
+(print '#\tab)
+(print '#\space)
+
+(print '|even this is a legal Lisp symbol!|)
+
+;; Reading and Printing Stuff the Way Humans Like It
+
+(princ '3)
+(princ '3.4)
+(princ 3.4)
+(princ 'foo)
+(princ '"foo")
+(princ "foo")
+(princ '#\a)
+(princ #\a)
+
+(progn (princ "This sentence will be interrupted")
+       (princ #\newline)
+       (princ "by an annoying newline character."))
+
+
+(defun say-hello ()
+  (princ "Please type your name:")
+  (let ((name (read-line)))
+    (princ "Nice to meet you, ")
+    (princ name)))
+
+(say-hello)
 

babb092d2215c5ee5424e9a15a18477b5e967f13
Kazuya NUMATA <numata@gmail.com>
2013-03-26 19:48:08 +0900

    Implement ch7/02-coping-with-complicated-data examples (in progress)

---
+++ ch7/02-coping-with-complicated-data.lisp
@@ -0,0 +1,17 @@
+(defparameter *house* '((wall (mortar
+			       (cement)
+			       (water)
+			       (sand))
+			 (bricks))
+			(window
+			 (glass)
+			 (frame)
+			 (curtains))
+			(root
+			 (shingles)
+			 (chimney))))
+
+;; Visualizing Graphs
+
+;; to be continued
+

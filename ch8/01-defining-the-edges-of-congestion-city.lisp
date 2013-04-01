578bf3131b9a8ddedb3d7dff1e90ff60ba62a406
Kazuya NUMATA <numata@gmail.com>
2013-04-01 16:57:41 +0900

    Implement ch8/01-defining-the-edges-of-congestion-city examples (in progress)

---
+++ ch8/01-defining-the-edges-of-congestion-city.lisp
@@ -0,0 +1,27 @@
+(load "graph-util")
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congestion-city-edges* nil)
+(defparameter *visited-nodes* nil)
+
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3)
+(defparameter *cop-odds* 15)
+
+;; Generating Random Edges
+
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num*
+		       collect (edge-pair (random-node) (random-node)))))
+(make-edge-list)
+
+;; Looping with the loop Command
+
+;; To be continued

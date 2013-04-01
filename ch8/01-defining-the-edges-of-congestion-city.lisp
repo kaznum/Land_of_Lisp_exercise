1f4d08428ccc3aeeecc44e3238695833ac0020d5
Kazuya NUMATA <numata@gmail.com>
2013-04-01 17:03:19 +0900

    Implement ch8/01-defining-the-edges-of-congestion-city examples (in progress)

--- ch8/01-defining-the-edges-of-congestion-city.lisp
+++ ch8/01-defining-the-edges-of-congestion-city.lisp
@@ -24,4 +24,16 @@
 
 ;; Looping with the loop Command
 
+(loop repeat 10
+     collect 1)
+
+(loop for n from 1 to 10
+     collect n)
+
+(loop for n from 1 to 10
+     collect (+ 100 n))
+
+;; Preventing Islands
+
+
 ;; To be continued

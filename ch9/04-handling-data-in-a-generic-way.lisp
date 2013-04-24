9123b017bb372b94415342d2aef5d3f446142eaf
Kazuya Numata <numata@gmail.com>
2013-04-25 08:46:01 +0900

    Implement ch9/04-handling-data-in-a-generic-way examples (in progress)

--- ch9/04-handling-data-in-a-generic-way.lisp
+++ ch9/04-handling-data-in-a-generic-way.lisp
@@ -12,7 +12,45 @@
 (every #'numberp '(a b c 5 d))
 
 ;; Sequence Functions for Iterating Across a Sequence
+(reduce #'+ '(3 4 6 5 2))
 
+(reduce (lambda (best item)
+	  (if (and (evenp item) (> item best))
+	      item
+	    best))
+	'(7 4 6 5 2)
+	:initial-value 0)
+
+(reduce (lambda (best item)
+	  (if (and (evenp item) (> item best))
+	      item
+	    best))
+	'(7 4 6 5 2))
+
+(defun sum (lst)
+  (reduce #'+ lst))
+
+(sum '(1 2 3))
+
+(sum (make-array 5 :initial-contents '(1 2 3 4 5)))
+
+(sum "hogehoge")
+
+(map 'list
+     (lambda (x)
+       (if (eq x #\s)
+	   #\S
+	 x))
+     "this is a string")
+
+(map 'string
+     (lambda (x)
+       (if (eq x #\s)
+	   #\S
+	 x))
+     "this is a string")
+
+;; Two More Important Sequence Functions
 
 ;; to be continued
 

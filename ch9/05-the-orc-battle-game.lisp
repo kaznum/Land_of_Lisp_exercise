d35f5323e7a10835db07cfdadd4d7d134b4f6d39
Kazuya NUMATA <numata@gmail.com>
2013-04-28 12:42:18 +0900

    Implement ch9/05-the-orc-battle-game examples (in progress)

---
+++ ch9/05-the-orc-battle-game.lisp
@@ -0,0 +1,48 @@
+;; Global Variables for the Player and Monsters
+(defparameter *player-health* nil)
+(defparameter *player-agility* nil)
+(defparameter *player-strength* nil)
+
+(defparameter *monsters* nil)
+(defparameter *monstaer-builders* nil)
+(defparameter *monster-num* 12)
+
+;; Main Game Functions
+
+(defun orc-battle ()
+  (init-monsters)
+  (init-player)
+  (game-loop)
+  (when (player-dead)
+    (princ "You have been killed. Game Over."))
+  (when (monsters-dead)
+    (princ "Congratulations! You have vanquished all of your foes.")))
+
+(defun game-loop ()
+  (unless (or (player-dead) (monsters-dead))
+    (show-player)
+    (dotimes (k (1+ (truncate (/ (max 0 *player-agility*) 15))))
+      (unless (monsters-dead)
+	(show-monsters)
+	(player-attack)))
+    (fresh-line)
+    (map 'list
+	 (lambda (m)
+	   (or (monster-dead m) (monster-attack m)))
+	 *monsters*)
+    (game-loop)))
+
+
+(dotimes (i 3)
+  (fresh-line)
+  (princ i)
+  (princ ". Hatchoo!"))
+
+
+;; Player Management Functions
+
+
+;; to be continue
+
+
+

665713f399a884e1ac1aaa04814c193c5c7d4e26
Kazuya NUMATA <numata@gmail.com>
2013-05-09 00:59:10 +0900

    Implement ch9/05-the-orc-battle-game examples (in progress)

--- ch9/05-the-orc-battle-game.lisp
+++ ch9/05-the-orc-battle-game.lisp
@@ -207,6 +207,22 @@
     (decf *player-health* x)))
 
 ;; The Slimy Slime Mold
+(defstruct (slime-mold (:include monster)) (sliminess (randval 5)))
+(push #'make-slime-mold *monster-builders*)
+
+(defmethod monster-show ((m slime-mold))
+  (princ "A slime mold with a sliminess of ")
+  (princ (slime-mold-sliminess m)))
+
+(defmethod monster-attack ((m slime-mold))
+  (let ((x (randval (slime-mold-sliminess m))))
+    (princ "A slime mold wraps around your legs and decreases your agility by ")
+    (princ x)
+    (princ "! ")
+    (decf *player-agility* x)
+    (when (zerop (random 2))
+      (princ "It also squirts in your face, taking away a health point! ")
+      (decf *player-health*))))
 
 ;; to be continue
 

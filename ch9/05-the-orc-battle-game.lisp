41a7b186bfe557b043a4ece649bbdd4b79f80df6
Kazuya NUMATA <numata@gmail.com>
2013-05-09 22:11:29 +0900

    Implement ch9/05-the-orc-battle-game examples (in progress)

--- ch9/05-the-orc-battle-game.lisp
+++ ch9/05-the-orc-battle-game.lisp
@@ -224,5 +224,21 @@
       (princ "It also squirts in your face, taking away a health point! ")
       (decf *player-health*))))
 
+(defstruct brigand (:include monster))
+(push #'make-brigand *monster-builders*)
+(defmethod monster-attack ((m brigand))
+  (let ((x (max *player-health* *player-agility* *player-strength*)))
+    (cond ((= x *player-health*)
+	   (princ "A brigand hits you with his slingshot, taking off 2 health points! ")
+	   (decf *player-health* 2))
+	  ((= x *player-agility*)
+	   (princ "A brigand catches your leg with his whip, taking off 2 agility points! ")
+	   (decf *player-agility* 2))
+	  ((= x *player-strength*)
+	   (princ "A brigand cuts your arms with his whips, taking off 2 strength points! ")
+	   (decf *player-strength* 2)))))
+
+;; To Battle!
+
 ;; to be continue
 

9fe755629a29b7a0e2b13ac276795a91f1eb3066
Kazuya Numata <numata@gmail.com>
2013-05-20 20:41:38 +0900

    Implement ch10/02-using-loop-to-evolve examples (in progress)

---
+++ ch10/02-using-loop-to-evolve.lisp
@@ -0,0 +1,8 @@
+(defparameter *width* 100)
+(defparameter *height* 30)
+(defparameter *jungle* '(45 10 10 10))
+(defparameter *plant-energy* 80)
+
+;; Glowing Plants in Our World
+
+;; to be continued

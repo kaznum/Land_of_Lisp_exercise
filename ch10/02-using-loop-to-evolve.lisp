77b9431cbb6148c20f98817f7b929893a12bd1bd
Kazuya Numata <numata@gmail.com>
2013-05-20 21:00:50 +0900

    Implement ch10/02-using-loop-to-evolve examples (in progress)

--- ch10/02-using-loop-to-evolve.lisp
+++ ch10/02-using-loop-to-evolve.lisp
@@ -5,4 +5,14 @@
 
 ;; Glowing Plants in Our World
 
+(defparameter *plants* (make-hash-table :test #'equal))
+(defun random-plant (left top width height)
+  (let ((pos (cons (+ left (random width)) (+ top (random height)))))
+    (setf (gethash pos *plants*) t)))
+(defun add-plants ()
+  (apply #'random-plant *jungle*)
+  (random-plant 0 0 *width* *height*))
+
+;; Creating Animals
+	
 ;; to be continued

b25766b9f23e8ff251502a0eb603829886d71bca
Kazuya NUMATA <numata@gmail.com>
2013-05-30 13:06:35 +0900

    Implement ch11/01-anatomy-of-the-format-function examples

---
+++ ch11/01-anatomy-of-the-format-function.lisp
@@ -0,0 +1,9 @@
+(format t "Add onion rings for only ~$ dollars more!" 1.5)
+
+;; The Destination Parameter
+(princ (reverse
+	(format t "Add onion rings for only ~$ dollars more!" 1.5)))
+
+;; The Control String Parameter
+
+;; Value Parameters

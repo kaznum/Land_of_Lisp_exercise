d0e500b3be243e7757cdfb6ffce85b0140ddf8f4
Kazuya NUMATA <numata@gmail.com>
2013-06-09 20:49:47 +0900

    Implement ch11/08-attack-of-the-robots examples

---
+++ ch11/08-attack-of-the-robots.lisp
@@ -0,0 +1,48 @@
+;;;
+;;; If there are more than 1 elements of the number, it is dead robots
+;;; Dead robots are '#' on console, which don't move and kill other robots when overrapped.
+;;;
+
+(defun robots ()
+  (loop named main
+     with directions = '((q . -65) (w . -64) (e . -63) (a . -1)
+			 (d .   1) (z .  63) (x .  64) (c . 65))
+     for pos = 544
+     then (progn (format t "~%qwe/asd/zxc to move, (t)eleport, (l)eave:")
+		 (force-output)
+		 (let* ((c (read))
+			(d (assoc c directions)))
+		   (cond (d (+ pos (cdr d)))
+			 ((eq 't c) (random 1024))
+			 ((eq 'l c) (return-from main 'bye))
+			 (t pos))))
+     for monsters = (loop repeat 10
+		       collect (random 1024))
+     then (loop for mpos in monsters
+	     collect (if (> (count mpos monsters) 1)
+			 mpos ;; Do not move if monsters are dead
+			 (cdar (sort (loop for (k . d) in directions
+					for new-mpos = (+ mpos d)
+					collect (cons (+ (abs (- (mod new-mpos 64)
+								 (mod pos 64)))
+							 (abs (- (ash new-mpos -6)
+								 (ash pos -6))))
+						      new-mpos))
+				     '<
+				     :key #'car))))
+     when (loop for mpos in monsters
+	     always (> (count mpos monsters) 1))  ;; All the points where a monster is have 2 or more monsters
+     return 'player-wins
+     do (format t
+		"~%|~{~<|~%|~,65:;~A~>~}|"
+		(loop for p
+		   below 1024
+		   collect (cond ((member p monsters)
+				  (cond ((= p pos) (return-from main 'player-loses))
+					((> (count p monsters) 1) #\#)
+					(t #\A)))
+				 ((= p pos)
+				  #\@)
+				 (t
+				  #\ ))))))
+

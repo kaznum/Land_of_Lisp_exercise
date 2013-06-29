a0d2bfd2b45042d3fdc891309dd51915a908af24
Kazuya NUMATA <numata@gmail.com>
2013-06-29 21:46:50 +0900

    Implement ch12/02-working-with-files examples (in progress)

---
+++ ch12/02-working-with-files.lisp
@@ -0,0 +1,15 @@
+(with-open-file (my-stream "data.txt" :direction :output)
+  (print "my data" my-stream))
+
+(with-open-file (my-stream "data.txt" :direction :input)
+  (read my-stream))
+
+(let ((animal-noises '((dog . woof)
+		       (cat . meow))))
+  (with-open-file (my-stream "animal-noises.txt" :direction :output)
+    (print animal-noises my-stream)))
+
+(with-open-file (my-stream "animal-noises.txt" :direction :input)
+    (read my-stream))
+
+

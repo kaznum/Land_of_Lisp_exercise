21719deba4bae75dcaf7d034e91a09bbb7322fb4
Kazuya NUMATA <numata@gmail.com>
2013-07-17 18:36:34 +0900

    Implement ch12/03-working-with-sockets examples (in progress)

---
+++ ch12/03-working-with-sockets.lisp
@@ -0,0 +1,20 @@
+;; Socket Addresses
+;; (nothing)
+;; Socket Connections
+;; (nothing)
+;; Sending a Message over a Socket
+(defparameter my-socket (socket-server 4321))  ;; server
+(defparameter my-stream (socket-accept my-socket)) ;; server
+
+(defparameter my-stream (socket-connect 4321 "127.0.0.1")) ;; client
+
+(print "Yo! Server!" my-stream) ;; client
+(read my-stream) ;; server
+
+(print "What up, client!" my-stream) ;; server
+(read my-stream) ;; client
+
+;; Tyding Up After Ourselves
+;;
+;; to be continued
+

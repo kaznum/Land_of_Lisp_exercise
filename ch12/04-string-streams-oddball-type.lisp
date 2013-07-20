f8b6cac035875e0bf655b0830be38cefe88be836
Kazuya NUMATA <numata@gmail.com>
2013-07-20 19:49:53 +0900

    Implement ch12/04-string-streams-oddball-type examples (in progress)

---
+++ ch12/04-string-streams-oddball-type.lisp
@@ -0,0 +1,9 @@
+(defparameter foo (make-string-output-stream))
+(princ "This will go into foo. " foo)
+(princ "This will also go into foo. " foo)
+(get-output-stream-string foo)
+
+;; Sending Streams to Functions
+
+;; to be continued
+

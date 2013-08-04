e082f31e738ee0bd692874bc2727a608b8729131
Kazuya NUMATA <numata@gmail.com>
2013-08-04 15:15:59 +0900

    Implement ch13/02-writing-a-web-server-from-scratch examples (in progress)

--- ch13/02-writing-a-web-server-from-scratch.lisp
+++ ch13/02-writing-a-web-server-from-scratch.lisp
@@ -1,3 +1,26 @@
 ;; How a Web Server Works
 ;; (no code)
 ;; Request Parameters
+(defun http-char (c1 c2 &optional (default #\Space))
+  (let ((code (parse-integer
+	       (coerce (list c1 c2) 'string)
+	       :radix 16
+	       :junk-allowed t)))
+    (if code
+	(code-char code)
+	default)))
+
+(defun decode-param (s)
+  (labels ((f (lst)
+	     (when lst
+	       (case (car lst)
+		 (#\% (cons (http-char (cadr lst) (caddr lst))
+			    (f (cdddr lst))))
+		 (#\+ (cons #\space (f (cdr lst))))
+		 (otherwise (cons (car lst) (f (cdr lst))))))))
+    (coerce (f (coerce s 'list)) 'string)))
+
+(decode-param "foo")
+(decode-param "foo%3F")
+
+;; to be continued

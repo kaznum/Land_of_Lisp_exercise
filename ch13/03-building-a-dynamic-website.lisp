2efe0e27efafd45b8ce727914b403a40c339b8eb
Kazuya NUMATA <numata@gmail.com>
2013-09-03 07:32:17 +0900

    Implement ch13/03-building-a-dynamic-website examples (in progress)

--- ch13/03-building-a-dynamic-website.lisp
+++ ch13/03-building-a-dynamic-website.lisp
@@ -2,10 +2,16 @@
   (if (equal path "greeting")
       (let ((name (assoc 'name params)))
 	(if (not name)
-	    (princ "<html><form>What is your name?<input name='name'> /></form></html>")
+	    (princ "<html><form>What is your name?<input name='name'/></form></html>")
 	    (format t "<html>Nice to meet you, ~a!</html>" (cdr name))))
       (princ "Sorry... I don't know that page.")))
 
 ;; Testing the Request Handler
+(hello-request-handler "lolcats" '() '())
 
+(hello-request-handler "greeting" '() '())
+
+(hello-request-handler "greeting" '() '((name . "Bob")))
+
+;; Launching the Website
 ;; to be continued

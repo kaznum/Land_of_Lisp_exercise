1a44dbaabefe59e95349d7e06e87fb8c54fce712
Kazuya NUMATA <numata@gmail.com>
2013-09-03 08:55:26 +0900

    Implement ch14/02-anatomy-of-a-program-written-in-the-functional-style examples (in progress)

---
+++ ch14/02-anatomy-of-a-program-written-in-the-functional-style.lisp
@@ -0,0 +1,12 @@
+;; the clean, functional part
+(defun add-widget (database widget)
+  (cons widget database))
+
+;; the dirty, nonfunctional part
+(defparameter *database* nil)
+
+(defun main-loop ()
+  (loop (princ "Please enter the name of a new widget:")
+     (setf *database* (add-widget *database* (read)))
+     (format t "The database contains the following: ~a~%" *database*)))
+

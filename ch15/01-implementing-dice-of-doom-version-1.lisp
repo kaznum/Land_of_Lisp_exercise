bac946e21cfac48f232e0643acba64f050b9627d
Kazuya NUMATA <numata@gmail.com>
2013-09-03 14:14:16 +0900

    Implement ch15/01-implementing-dice-of-doom-version-1 examples (in progress)

---
+++ ch15/01-implementing-dice-of-doom-version-1.lisp
@@ -0,0 +1,11 @@
+;; Defining Some Global Variables
+
+(defparameter *num-players* 2)
+(defparameter *max-dice* 3)
+(defparameter *board-size* 2)
+(defparameter *board-hexnum* (* *board-size* *board-size*))
+
+;; Representing the Game Board
+
+;; to be continued
+

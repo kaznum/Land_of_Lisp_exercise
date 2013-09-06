c2a5d7954fcffc676d8c528998afc877a8f8f01f
Kazuya NUMATA <numata@gmail.com>
2013-09-06 14:02:55 +0900

    Implement ch17/01-writing-svg-files examples (in progress)

--- ch17/01-writing-svg-files.lisp
+++ ch17/01-writing-svg-files.lisp
@@ -1,3 +1,15 @@
+;;;;; From Previous Chapter
+(defun pairs (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (if tail
+                        (f (cdr tail) (cons (cons head (car tail)) acc))
+                        (reverse acc))
+                    (reverse acc))))
+    (f lst nil)))
+
+
+;;; This Chapter
 ;; Creating XML and HTML with the tag Macro
 
 (defun print-tag (name alst closingp)
@@ -13,5 +25,29 @@
 (print-tag 'mytag '((color . blue) (height . 9)) nil)
 
 ;;; Creating the tag Macro
+;; (tag mytag (color 'blue height (+ 4 5)))
+;; -> <mytag color="BLUE" height="9"></mytag>
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+		     (list ,@(mapcar (lambda (x)
+				       `(cons ',(car x) ,(cdr x)))
+				     (pairs atts)))
+		     nil)
+	  ,@body
+	  (print-tag ',name nil t)))
+
+(macroexpand '(tag mytag (color 'blue height (+ 4 5))))
+
+(tag mytag (color 'blue size 'big)
+  (tag first_inner_tag ())
+  (tag second_inner_tag ()))
+
+;; <mytag color ="BLUE" size ="BIG">
+;;   <first_inner_tag></first_inner_tag>
+;;   <second_inner_tag></second_inner_tag>
+;; </mytag>
+
+;;; Using the tag Macro to Generate HTML
 
 ;; to be continued

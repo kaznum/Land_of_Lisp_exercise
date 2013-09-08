d5e8d6299cc32f3eb55a6c130342059c7faa2ea2
Kazuya NUMATA <numata@gmail.com>
2013-09-09 08:29:29 +0900

    Implement ch17/02-creating-custom-game-commands-for-wizards-adventure-game examples (in progress)

--- ch17/02-creating-custom-game-commands-for-wizards-adventure-game.lisp
+++ ch17/02-creating-custom-game-commands-for-wizards-adventure-game.lisp
@@ -21,5 +21,55 @@
 (pushnew 'weld *allowed-commands*)
 
 ;;; A Command for Dunking
+(setf *bucket-filled* nil)
+(defun dunk (subject object)
+  (if (and (eq *location* 'garden)
+	   (eq subject 'bucket)
+	   (eq object 'well)
+	   (have 'bucket)
+	   *chain-welded*)
+      (progn (setf *bucket-filled* 't)
+	     '(the bucket is now full of water))
+      '(you cannot dunk like that.)))
+
+(pushnew 'dunk *allowed-commands*)
+
+;;; The game-action Macro
+(defmacro game-action (command subj obj place &body body)
+  `(progn (defun ,command (subject object)
+	    (if (and (eq *location* ',place)
+		     (eq subject ',subj)
+		     (eq object ',obj)
+		     (have ',subj))
+		,@body
+		'(i cant ,command like that.)))
+	  (pushnew ',command *allowed-commands*)))
+
+(defparameter *chain-welded* nil)
+
+(game-action weld chain bucket attic
+  (if (and (have 'bucket) (not *chain-welded*))
+      (progn (setf *chain-welded* 't)
+	     '(the chain is now securely welded to the bucket.))
+      '(you do not have a bucket.)))
+
+(setf *bucket-filled* nil)
+
+(game-action dunk bucket well garden
+  (if *chain-welded*
+      (progn (setf *bucket-filled* 't)
+	     '(the bucket is now full of water))
+      '(the water level is too low to reach.)))
+
+(game-action splash bucket wizard living-room
+  (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
+	((have 'frog) '(the wizard awakens and sees that you stole his frog.
+			he is so upset he banishes you to the
+			netherworlds- you lose! the end.))
+	(t '(the wizard awakens from his slumber and greets you warmly.
+	     he hands you the magick low-carb donut- you win! the end.))))
+
+;; Let's Try the Completed Wizard's Adventure Game!
+
 
 ;; to be continued

1b22a5dfc9ad81c8b9467b63f38e08d179905c6a
Kazuya NUMATA <numata@gmail.com>
2013-09-09 08:47:07 +0900

    Make game-action escape the variable name collision

--- ch17/02-creating-custom-game-commands-for-wizards-adventure-game.lisp
+++ ch17/02-creating-custom-game-commands-for-wizards-adventure-game.lisp
@@ -36,14 +36,16 @@
 
 ;;; The game-action Macro
 (defmacro game-action (command subj obj place &body body)
-  `(progn (defun ,command (subject object)
-	    (if (and (eq *location* ',place)
-		     (eq subject ',subj)
-		     (eq object ',obj)
-		     (have ',subj))
-		,@body
-		'(i cant ,command like that.)))
-	  (pushnew ',command *allowed-commands*)))
+  (let ((subject (gensym))
+	(object (gensym)))
+    `(progn (defun ,command (,subject ,object)
+	      (if (and (eq *location* ',place)
+		       (eq ,subject ',subj)
+		       (eq ,object ',obj)
+		       (have ',subj))
+		  ,@body
+		  '(i cant ,command like that.)))
+	  (pushnew ',command *allowed-commands*))))
 
 (defparameter *chain-welded* nil)
 

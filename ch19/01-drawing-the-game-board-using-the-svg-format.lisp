a99b76949988785ba547538e64430a3135f7a016
Kazuya NUMATA <numata@gmail.com>
2013-09-10 08:48:55 +0900

    Implement ch19/01-drawing-the-game-board-using-the-svg-format example

---
+++ ch19/01-drawing-the-game-board-using-the-svg-format.lisp
@@ -0,0 +1,15 @@
+(load "dice_of_doom_v2")
+(load "webserver.lisp")
+(load "svg")
+
+(defparameter *board-width* 900)
+(defparameter *board-height* 500)
+(defparameter *board-scale* 64)
+(defparameter *top-offset* 3)
+(defparameter *dice-scale* 40)
+(defparameter *dot-size* 0.05)
+
+;; Drawing a Die
+
+;; to be continued
+

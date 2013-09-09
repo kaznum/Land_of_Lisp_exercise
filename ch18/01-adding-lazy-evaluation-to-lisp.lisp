8c5377e16cc79c5c765085635472f350f9923487
Kazuya NUMATA <numata@gmail.com>
2013-09-09 11:38:40 +0900

    Implement ch18/01-adding-lazy-evaluation-to-lisp examples (in progress)

--- ch18/01-adding-lazy-evaluation-to-lisp.lisp
+++ ch18/01-adding-lazy-evaluation-to-lisp.lisp
@@ -6,3 +6,33 @@
 (force (lazy (+ 1 2)))
 ;;; 3
 
+(defun add (a b)
+  (princ "I am adding now")
+  (+ a b))
+
+;;; The followings don't work right now
+(defparameter *foo* (lazy (add 1 2)))
+(force *foo*)
+
+(defmacro lazy (&body body)
+  (let ((forced (gensym))
+	(value (gensym)))
+    `(let ((,forced nil)
+	   (,value nil))
+       (lambda ()
+	 (unless ,forced
+	   (setf ,value (progn ,@body))
+	   (setf ,forced t))
+	 ,value))))
+
+(defun force (lazy-value)
+  (funcall lazy-value))
+
+;;; test
+(defparameter *bar* (lazy (add 1 2)))
+(force *bar*)
+(force *bar*)
+
+;; Creating a Lazy Lists Library
+
+;; to be continued

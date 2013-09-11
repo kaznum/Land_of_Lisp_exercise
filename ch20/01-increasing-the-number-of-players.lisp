3e446dc06c6da76542a255f4a02b136ddc2d1a2b
Kazuya NUMATA <numata@gmail.com>
2013-09-11 10:19:00 +0900

    Implement ch20/01-increasing-the-number-of-players examples

---
+++ ch20/01-increasing-the-number-of-players.lisp
@@ -0,0 +1,7 @@
+(load "dice_of_doom_v3")
+
+(defparameter *num-players* 4)
+(defparameter *die-colors* '((255 63 63) (63 63 255) (63 255 63) (255 63 255)))
+
+(defparameter *max-dice* 5)
+(defparameter *ai-level* 2)
